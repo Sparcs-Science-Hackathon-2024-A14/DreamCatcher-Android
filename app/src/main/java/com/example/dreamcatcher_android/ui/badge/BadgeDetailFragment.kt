4a1de01bc7d67c5b966e99b78142ac9e0807d06d
김성민 <arcn1345@gmail.com>
2024-10-19 02:30:40 +0900

    [chore] : BadgeDetailFragment 추가

---
+++ app/src/main/java/com/example/dreamcatcher_android/ui/badge/BadgeDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.dreamcatcher_android.ui.badge
+
+import android.util.Log
+import com.example.dreamcatcher_android.R
+import com.example.dreamcatcher_android.base.BaseFragment
+import com.example.dreamcatcher_android.databinding.FragmentBadgeDetailBinding
+
+class BadgeDetailFragment : BaseFragment<FragmentBadgeDetailBinding>(R.layout.fragment_badge_detail) {
+
+    override fun setLayout() {
+        start()
+    }
+
+    fun start() {
+        Log.d("fhrm", "dd")
+    }
+
+}
