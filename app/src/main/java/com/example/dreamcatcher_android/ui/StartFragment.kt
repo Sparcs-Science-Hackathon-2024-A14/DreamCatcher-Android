2141f6be47161d96e7311d00ae4db086925169e6
ësungmin <arcn1345@gmail.com>
2024-10-18 21:08:24 +0900

    [feat] : StartFragment 추가 및 UI

---
+++ app/src/main/java/com/example/dreamcatcher_android/ui/StartFragment.kt
@@ -0,0 +1,18 @@
+package com.example.dreamcatcher_android.ui
+
+import android.util.Log
+import com.example.dreamcatcher_android.R
+import com.example.dreamcatcher_android.base.BaseFragment
+import com.example.dreamcatcher_android.databinding.FragmentStartBinding
+
+class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start) {
+
+    override fun setLayout() {
+        start()
+    }
+
+    fun start() {
+        Log.d("로그","ㅇ")
+    }
+
+}
