e7f913e518af2d74398f2950ce08b42a9740cf8c
ësungmin <arcn1345@gmail.com>
2024-10-18 21:09:28 +0900

    [feat] : SignInFragment 추가 및 UI

---
+++ app/src/main/java/com/example/dreamcatcher_android/ui/SignInFragment.kt
@@ -0,0 +1,19 @@
+package com.example.dreamcatcher_android.ui
+
+import android.util.Log
+import com.example.dreamcatcher_android.R
+import com.example.dreamcatcher_android.base.BaseFragment
+import com.example.dreamcatcher_android.databinding.FragmentSignInBinding
+
+class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+    override fun setLayout() {
+        start()
+    }
+
+    fun start() {
+        Log.d("로그","ㅇ")
+    }
+
+
+}
