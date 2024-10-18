3590da8ada39ff56373eff0521e0ad1127deddc6
ësungmin <arcn1345@gmail.com>
2024-10-18 16:34:18 +0900

    [feat] : 홈 프래그먼트 추가

---
+++ app/src/main/java/com/example/dreamcatcher_android/ui/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.dreamcatcher_android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dreamcatcher_android.R
+import com.example.dreamcatcher_android.base.BaseFragment
+import com.example.dreamcatcher_android.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun setLayout() {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/example/dreamcatcher_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/dreamcatcher_android/ui/MainActivity.kt
@@ -1,27 +1,13 @@
 package com.example.dreamcatcher_android.ui
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.dreamcatcher_android.R
 import com.example.dreamcatcher_android.base.BaseActivity
 import com.example.dreamcatcher_android.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    override fun setLayout() {
+
     }
 
-    override fun setLayout() {
-        TODO("Not yet implemented")
-    }
 }
