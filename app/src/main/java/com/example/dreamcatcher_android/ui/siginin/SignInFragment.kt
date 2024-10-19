b4fae9ca96a77ade924c838b0a288d6df8bfd58b
ësungmin <arcn1345@gmail.com>
2024-10-20 06:25:11 +0900

    [feat] : SingInFragment 로직 수정

--- app/src/main/java/com/example/dreamcatcher_android/ui/siginin/SignInFragment.kt
+++ app/src/main/java/com/example/dreamcatcher_android/ui/siginin/SignInFragment.kt
@@ -1,14 +1,27 @@
 package com.example.dreamcatcher_android.ui.siginin
 
+import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.dreamcatcher_android.R
 import com.example.dreamcatcher_android.base.BaseFragment
 import com.example.dreamcatcher_android.databinding.FragmentSignInBinding
+import com.example.dreamcatcher_android.ui.MainViewModel
+import com.example.dreamcatcher_android.util.GlobalApplication
+import com.example.dreamcatcher_android.util.PreferenceUtil
 import com.example.dreamcatcher_android.util.navigateSafe
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun setLayout() {
         initSettings()
@@ -21,8 +34,31 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
 
     private fun initButtons() {
         binding.fragmentSigninLoginBtn.setOnClickListener {
-            val action = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
-            findNavController().navigateSafe(action.actionId)
+            val name = binding.fragmentSigninNameEt.text.toString()
+            val age = binding.fragmentSigninAgeEt.text.toString().toInt()
+            getSignIn(name, age)
+//            val action = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
+//            findNavController().navigateSafe(action.actionId)
+        }
+    }
+
+    private fun getSignIn(name: String, age: Int) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.getLogin(name, age)
+                mainViewModel.loginResponse.collect { res ->
+                    if (res.isSuccessful && res.code() == 200) {
+                        val userId = res.body()?.id?.toInt()
+                        if (userId != null){
+                            res.body()?.id?.let { GlobalApplication.prefsManager.saveUserId(userId) } // id 저장 -> 뱃지 조회
+                            if(GlobalApplication.prefsManager.getUserId() != 0) {
+                                val action = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
+                                findNavController().navigateSafe(action.actionId)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
