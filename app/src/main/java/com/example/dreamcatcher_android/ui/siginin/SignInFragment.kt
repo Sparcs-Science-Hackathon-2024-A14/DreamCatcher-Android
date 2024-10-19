83bfde5d6382ceb2e4e14b9a11f91074dc28e403
ësungmin <arcn1345@gmail.com>
2024-10-19 12:54:41 +0900

    [feat] : SingInFragment 로그인 버튼 활성화/비활성화 코드 추가

--- app/src/main/java/com/example/dreamcatcher_android/ui/siginin/SignInFragment.kt
+++ app/src/main/java/com/example/dreamcatcher_android/ui/siginin/SignInFragment.kt
@@ -1,5 +1,7 @@
 package com.example.dreamcatcher_android.ui.siginin
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.navigation.fragment.findNavController
 import com.example.dreamcatcher_android.R
 import com.example.dreamcatcher_android.base.BaseFragment
@@ -13,14 +15,41 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
     }
 
     private fun initSettings() {
+        activateSignInButton()
         initButtons()
     }
 
     private fun initButtons() {
-        binding.fragmentSigninLoginIb.setOnClickListener {
+        binding.fragmentSigninLoginBtn.setOnClickListener {
             val action = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
             findNavController().navigateSafe(action.actionId)
         }
+    }
+
+    private fun activateSignInButton() {
+        val disableBackground = R.drawable.ic_pupler5_btn
+        val enableBackground = R.drawable.ic_gradation_btn
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val nameText = binding.fragmentSigninNameEt.text.toString().trim()
+                val ageText = binding.fragmentSigninAgeEt.text.toString().trim()
+
+                if(nameText.isNotEmpty() && ageText.isNotEmpty()) {
+                    binding.fragmentSigninLoginBtn.setBackgroundResource(enableBackground)
+                    binding.fragmentSigninLoginBtn.isEnabled = true
+                } else {
+                    binding.fragmentSigninLoginBtn.setBackgroundResource(disableBackground)
+                    binding.fragmentSigninLoginBtn.isEnabled = true
+                }
+            }
+        }
+        binding.fragmentSigninNameEt.addTextChangedListener(textWatcher)
+        binding.fragmentSigninAgeEt.addTextChangedListener(textWatcher)
     }
 
 }
