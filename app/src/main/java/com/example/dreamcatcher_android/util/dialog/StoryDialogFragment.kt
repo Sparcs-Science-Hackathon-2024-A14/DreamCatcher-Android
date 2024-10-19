3208f59149b4112a8646dbc6883003760fdc06ad
ësungmin <arcn1345@gmail.com>
2024-10-19 23:10:25 +0900

    [feat] : StoryDialogFragment 클릭 리스너 및 UI 코드 추가

--- app/src/main/java/com/example/dreamcatcher_android/util/dialog/StoryDialogFragment.kt
+++ app/src/main/java/com/example/dreamcatcher_android/util/dialog/StoryDialogFragment.kt
@@ -1,8 +1,10 @@
 package com.example.dreamcatcher_android.util.dialog
 
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +13,27 @@ import com.bumptech.glide.Glide
 import com.example.dreamcatcher_android.R
 import com.example.dreamcatcher_android.databinding.DialogStoryBinding
 
-class StoryDialogFragment(private val imageUrl: String, title: String, content: String) :
+class StoryDialogFragment(
+    private val imageUrl: String,
+    title: String,
+    content: String,
+    private val clickListener: DialogClickListener
+) :
     DialogFragment() {
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        clickListener.onDialogDismiss() // 다이얼로그가 닫힐 때 MapFragment에서 핸들러를 다시 시작
+    }
 
     override fun onStart() {
         super.onStart()
         val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
         dialog?.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setGravity(Gravity.TOP)
+        val params = dialog?.window?.attributes
+        params?.y = 100 // 위쪽 마진을 100dp로 설정 (값을 조정하여 마진을 설정)
+        dialog?.window?.attributes = params
     }
 
     // 뷰 바인딩
@@ -47,6 +63,7 @@ class StoryDialogFragment(private val imageUrl: String, title: String, content: 
 
         // 확인 버튼 클릭
         binding.dialogStoryStartBt.setOnClickListener {
+            clickListener.onBtnClick1()
             dismiss()
         }
 
